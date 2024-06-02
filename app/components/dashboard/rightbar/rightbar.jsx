d86858b91026b848e920d1c608c7d0cc7a9e5a97
saurav-ghimire <sauravghimire0123@gmail.com>
2024-06-02 11:06:50 -0400

    right bar design created

--- app/components/dashboard/rightbar/rightbar.jsx
+++ app/components/dashboard/rightbar/rightbar.jsx
@@ -1,5 +1,42 @@
+import Image from 'next/image';
+import styles from './rightbar.module.css'
+import { MdPlayCircleFilled } from 'react-icons/md';
 function Rightbar() {
-  return ( "Rightbar" );
+  return ( 
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src={'/astronaut.png'} alt='' fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>️‍🔥 Available Now</span>
+          <h3 className={styles.title}>How to use new version of admin dashboard</h3>
+          <span className={styles.subTitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+          <button className={styles.button}><MdPlayCircleFilled/> Watch</button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src={'/astronaut.png'} alt='' fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🚀 Comming Soon</span>
+          <h3 className={styles.title}>How to use new version of admin dashboard</h3>
+          <span className={styles.subTitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+          <button className={styles.button}><MdPlayCircleFilled/> Watch</button>
+        </div>
+      </div>
+
+     
+    </div>
+   );
 }
 
 export default Rightbar;
