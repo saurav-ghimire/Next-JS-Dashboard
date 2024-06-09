168d5aabc1d32491452b9aa5048021ca41a34c70
saurav-ghimire <sauravghimire0123@gmail.com>
2024-06-09 18:35:31 -0400

    Auth JS setup

---
+++ middleware.js
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+import { authconfig } from './authconfig';
+
+ 
+export default NextAuth(authconfig).auth;
+ 
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
